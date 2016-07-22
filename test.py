42d2c883fd78d75bae5cb6f08f375313e7e9231a
vipercf <540910693@qq.com>
2016-07-22 16:23:44 +0800

    add test

---
+++ test.py
@@ -0,0 +1,8 @@
+# -*- coding: UTF-8 -*- 
+# File Name: test.py
+# Author: vipercf
+# mail: vipercf@163.com
+# Created Time: 五  7/22 16:22:19 2016
+#########################################################################
+#!/usr/bin/env python
+print "Hello world!"
